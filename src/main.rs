57aff1186d17aa2c99ce7a6e100b47f8f88f8d08
Nia Watts <niawatts95@gmail.com>
2022-08-15 00:29:00 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::time;
+use std::{net::SocketAddr, thread};
+
+use axum::{routing::get, Router};
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/works", get(works))
+        .route("/fails", get(fails));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn works() -> &'static str {
+    tokio::task::spawn_blocking(|| {
+        info!("Starting works request");
+        let ten_millis = time::Duration::from_secs(3);
+
+        thread::sleep(ten_millis);
+        info!("Ending works request");
+        "Delayed Hello, World!"
+    })
+    .await
+    .unwrap()
+}
+
+async fn fails() -> &'static str {
+    info!("Starting fails request");
+    let ten_millis = time::Duration::from_secs(3);
+
+    thread::sleep(ten_millis);
+    info!("Ending fails request");
+    "Delayed Hello, World!"
+}
